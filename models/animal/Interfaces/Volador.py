b8f36b391c28e19ac9b7501e5c758cc28aca876d
Daniel Garibello <danielgaribelloescobar@gmail.com>
2026-04-28 04:28:19 +0200

    Taller completo - Punto 8 corregido y Motor arreglado

--- models/animal/Interfaces/Volador.py
+++ models/animal/Interfaces/Volador.py
@@ -3,4 +3,5 @@ from abc import ABC, abstractmethod
 class Volador(ABC):
     @abstractmethod
     def volar(self):
+        """Interfaz para definir la acción de volar"""
         pass
--- models/motor.py
+++ models/motor.py
@@ -1,22 +1,16 @@
 class Motor:
-    # Definimos los atributos básicos del motor [cite: 85]
+    # Agregamos 'velocidad' aquí para que acepte los 3 datos del main
     def __init__(self, potencia, tipo, velocidad):
         self.__potencia = potencia
         self.__tipo = tipo
         self.__velocidad = velocidad
 
-    # Método para devolver los datos del motor [cite: 87]
     def obtener_info_motor(self):
         return f"Motor {self.__tipo} con {self.__potencia} HP"
     
-    # metodo aumentar velocidad del coche
     def incrementar_velocidad(self):
         if self.__velocidad < 0:
-            raise ValueError ("la velocidad no puede ser negativa")
+            raise ValueError("la velocidad no puede ser negativa")
         if self.__velocidad >= 200:
             return f"Velocidad maxima excedida: {self.__velocidad}"
         return self.__velocidad + 5
-
-        
-    
-        
